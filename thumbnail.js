b6746802d6118c78873ee5c8bb500d2d5ba869f7
signinUser-0222 <100207246+signinUser-0222@users.noreply.github.com>
2022-02-25 13:39:18 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,8 +1,10 @@
-let targetImage = document.querySelector("#smart-image");
-targetImage.addEventListener("click", function() {
-    if (targetImage.classList.contains("small")) {
-        targetImage.classList.remove("small");
-    } else {
-        targetImage.classList.add("small");
-    }
+document.addEventListener("DOMContentLoaded", function(event) {
+        let targetImage = document.querySelector("#smart-image");
+        targetImage.addEventListener("click", function() {
+        if (targetImage.classList.contains("small")) {
+            targetImage.classList.remove("small");
+        } else {
+            targetImage.classList.add("small");
+        }
+    });
 });
