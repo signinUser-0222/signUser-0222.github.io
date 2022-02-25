b8b0a29da8e1de6ca887c9e3bd90209e640b9a84
signinUser-0222 <100207246+signinUser-0222@users.noreply.github.com>
2022-02-25 13:28:29 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
 });
 let targetImage = document.querySelector("#smart-image");
-targetImage.addEventListener("click", function() {
+targetImage.addEventListener("onclick", function() {
     if (targetImage.classList.contains("small")) {
         targetImage.classList.remove("small");
     } else {
